efd221fde38895cf3a16edfc776f9879f9eb0cd3
Wagner Ferreira <wagner-ferreira09@hotmail.com>
2022-08-31 23:45:47 -0300

    CRUD Scaffolding

---
+++ ProjetoSalesWebMVC/ProjetoSalesWebMVC/Data/ProjetoSalesWebMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoSalesWebMVC.Models;
+
+namespace ProjetoSalesWebMVC.Data
+{
+    public class ProjetoSalesWebMVCContext : DbContext
+    {
+        public ProjetoSalesWebMVCContext (DbContextOptions<ProjetoSalesWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoSalesWebMVC.Models.Department> Department { get; set; }
+    }
+}
