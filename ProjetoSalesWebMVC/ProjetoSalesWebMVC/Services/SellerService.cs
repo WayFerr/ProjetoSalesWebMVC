ebc748195d72cd42bb73b3accf852635570bc989
Wagner Ferreira <wagner-ferreira09@hotmail.com>
2022-09-03 23:03:04 -0300

    SellerService and basic FindAll

---
+++ ProjetoSalesWebMVC/ProjetoSalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using ProjetoSalesWebMVC.Data;
+using ProjetoSalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoSalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly ProjetoSalesWebMVCContext _context;
+
+        public SellerService(ProjetoSalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- ProjetoSalesWebMVC/ProjetoSalesWebMVC/Startup.cs
+++ ProjetoSalesWebMVC/ProjetoSalesWebMVC/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ProjetoSalesWebMVC.Data;
+using ProjetoSalesWebMVC.Services;
 
 namespace ProjetoSalesWebMVC
 {
@@ -40,6 +41,7 @@ namespace ProjetoSalesWebMVC
                     options.UseMySql(Configuration.GetConnectionString("ProjetoSalesWebMVCContext"), builder => builder.MigrationsAssembly("ProjetoSalesWebMVC")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
