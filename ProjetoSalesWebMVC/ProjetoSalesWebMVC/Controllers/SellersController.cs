1c83909427f06ac74c622e36c53420ad922d2a6d
Wagner Ferreira <wagner-ferreira09@hotmail.com>
2022-09-05 21:49:27 -0300

    Validation

--- ProjetoSalesWebMVC/ProjetoSalesWebMVC/Controllers/SellersController.cs
+++ ProjetoSalesWebMVC/ProjetoSalesWebMVC/Controllers/SellersController.cs
@@ -36,6 +36,12 @@ namespace ProjetoSalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -96,6 +102,12 @@ namespace ProjetoSalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
